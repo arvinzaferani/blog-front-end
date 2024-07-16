9fcb91250570ef0684d1450067a122ed93217509
Arvinito <arzaferani@gmail.com>
2024-07-16 15:57:18 +0330

    Setup data reducer for reduce sliced data

---
+++ src/store/index.module.ts
@@ -0,0 +1,11 @@
+import {configureStore} from "@reduxjs/toolkit";
+import dataReducer from "../features/dataSlice";
+const store = configureStore({
+    reducer: {
+        data: dataReducer,
+    }
+})
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
