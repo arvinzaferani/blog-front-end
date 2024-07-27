e63c1fa132af759507c3dbff3b297354c7a21d61
Arvinito <arzaferani@gmail.com>
2024-07-27 20:38:15 +0330

    Define Signup reducer in store index files

--- src/store/index.module.ts
+++ src/store/index.module.ts
@@ -1,8 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
-import dataReducer from "../features/dataSlice";
+import dataReducer from "../features/postsSlice";
+import signupReducer from "../features/signupSlice"
+
 const store = configureStore({
     reducer: {
         data: dataReducer,
+        signup: signupReducer
     }
 })
 export type RootState = ReturnType<typeof store.getState>
