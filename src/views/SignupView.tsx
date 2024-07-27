f0f4cd8fd37aabdb88a33ba80417ef0f4b8ede70
Arvinito <arzaferani@gmail.com>
2024-07-27 20:41:12 +0330

    Add signupView file

---
+++ src/views/SignupView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SignupComponent from "../components/SignupComponent";
+
+const SignupView:React.FC = () => {
+    return(
+        <SignupComponent/>
+    )
+}
+export default SignupView
