fa9bb2e27b3f10040584ee9a4c454f6136bfacbc
Arvinito <arzaferani@gmail.com>
2025-02-10 11:39:47 +0330

    Update Axios API configuration

--- src/service/axiosConfig.ts
+++ src/service/axiosConfig.ts
@@ -1,12 +1,31 @@
 import axios, {InternalAxiosRequestConfig, AxiosInstance} from "axios";
 const apiClient:AxiosInstance = axios.create({
-    baseURL: process.env.BAE_URL,
+    baseURL: 'http://localhost:6969',
+    timeout: 1500,
 })
+
 const axiosConfig = (config: InternalAxiosRequestConfig) => {
     const token: string | null = localStorage.getItem('token')
     if (token)
         config.headers.Authorization = `Bearer ${token}`
     return config
 }
-apiClient.interceptors.request.use(axiosConfig, (error) => Promise.reject(error) )
+
+apiClient.interceptors.request.use(axiosConfig,
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    (response) => {
+        if (response.data?.jwt)
+            localStorage.setItem('token', response.data.jwt)
+        return response.data;
+
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
+);
 export default apiClient;
